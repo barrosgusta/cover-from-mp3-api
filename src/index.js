ccf4e38f5b7a5a18f9b230a64b9b4801732a7f2b
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-11-26 17:16:11 -0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import cors from 'cors';
+import multer from 'multer';
+import ffmpeg from 'fluent-ffmpeg';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './src/songs');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "_" + file.originalname.replace(/\s/g, '').replace(/.mp3/g, '').toLowerCase() + '.mp3');
+    }
+});
+
+var upload = multer({ storage: storage });
+
+app.post('/cover-from-mp3', upload.single("audio"), function (req, res) {
+    console.log(req.file);
+    if (!req.file) {
+        res.status(400).send('No file uploaded.');
+        return;
+    }
+    var command = ffmpeg();
+    command
+        .input(req.file.path)
+        .noAudio()
+        .output('output.jpg')
+        .on('end', function () {
+        res.download('output.jpg');
+    })
+        .on('error', function (err) {
+        console.log('An error occurred: ' + err.message);
+        res.status(500).send(err.message);
+    })
+        .run();
+});
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
